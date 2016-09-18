529fd5c1352783a414fbb0ce940c2e983d86233e
chenhao <chenhao@microduino.cc>
2016-09-18 17:19:59 +0800

    run on meteor 1.4.1.1 and ios 10

--- mCottonV2/mCotton-web/start_ios.sh
+++ mCottonV2/mCotton-web/start_ios.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-meteor run ios-device --production --verbose --settings ./setting/web.json --mobile-server http://dev.microduino.cn
+meteor run ios-device --production --verbose --port 9800 --settings ./setting/web.json --mobile-server http://192.168.199.240:9800
